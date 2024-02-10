885eca95368e3532ed7295270c2a94b48e5b1b2b
Aboubakr <abouross69@gmail.com>
2024-02-10 14:08:33 +0100

    discovery & gateway services setup

---
+++ gateway-service/src/main/java/aboudev/cms/gateway/configs/Access.java
@@ -0,0 +1,16 @@
+package aboudev.cms.gateway.configs;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class Access {
+    private String path;
+    private String method;
+    private List<String> roles;
+    private String resource;
+    private String adminRole;
+}
---
+++ gateway-service/src/main/java/aboudev/cms/gateway/configs/AppConfigs.java
@@ -0,0 +1,17 @@
+package aboudev.cms.gateway.configs;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+@ConfigurationProperties(prefix = "app")
+@Getter
+@Setter
+public class AppConfigs {
+    private List<String> corsOrigins;
+    private List<Access> accesses;
+}
